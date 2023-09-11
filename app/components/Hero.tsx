4b567f940a289c924cac74d0e99ab858617bb238
petrikur <patekuronen123@gmail.com>
2023-09-11 20:47:35 +0300

    hero image

---
+++ app/components/Hero.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import Image from 'next/image';
+import heroimg1 from '../../app/images/heroimg1.jpg';
+
+const HeroSection = () => {
+  return (
+    <section className="relative">
+      <div className="relative w-full h-96 md:h-[600px] lg:h-[700px]">
+        <Image
+          src={heroimg1}
+          alt="Fashion Model"
+          layout="fill"
+          objectFit="cover"
+          className="rounded-lg shadow-lg"
+        />
+      </div>
+      {/* Text Overlay */}
+      <div className="absolute md:top-40 top-16 left-0 w-full h-full bg-gradient-to-r  p-8 text-white">
+        <div className="container mx-auto">
+          <h1 className="text-4xl md:text-6xl font-extrabold leading-tight mb-4">
+            Discover Your Style
+          </h1>
+          <p className="text-lg md:text-xl mb-8">
+            Shop the latest trends in fashion. Find your perfect look today!
+          </p>
+          <a
+            href="/shop"
+            className="bg-white text-indigo-500 hover:bg-indigo-600 text-lg md:text-xl px-6 py-3 rounded-full inline-block transition duration-300"
+          >
+            Shop Now
+          </a>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default HeroSection;
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import HeroSection from "./components/Hero";
 import Header from "./components/layout/Header";
 
 export default function Home() {
   return (
     <>
       <Header />
+    <HeroSection />
     </>
   );
 }
