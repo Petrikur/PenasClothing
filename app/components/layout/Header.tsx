6c27fcb969eda63d3468cd15afbb8b3ad0fbb8e5
petrikur <patekuronen123@gmail.com>
2023-09-13 20:48:21 +0300

    fix header

--- app/components/layout/Header.tsx
+++ app/components/layout/Header.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { TiShoppingCart } from 'react-icons/ti';
+import { BsCart } from 'react-icons/bs';
+import { FaSearch } from 'react-icons/fa';
 
 interface HeaderProps {}
 
@@ -11,34 +12,37 @@ const Header: React.FC<HeaderProps> = () => {
           Logo here
         </div>
         <nav className="md:block hidden">
-          <ul className="flex space-x-6 ">
+          <ul className="flex space-x-6">
             <li><a href="/">Home</a></li>
             <li><a href="/shop">Shop</a></li>
             <li><a href="/categories">Categories</a></li>
           </ul>
         </nav>
         <div className="flex items-center space-x-2 mt-4 md:mt-0 md:ml-4">
-          <input
-            type="text"
-            placeholder="Search products..."
-            className="border border-gray-300 rounded px-2 py-1"
-          />
+          <div className="relative">
+            <FaSearch className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400" />
+            <input
+              type="text"
+              placeholder="Search products..."
+              className="border border-gray-400 pl-10 pr-2 py-1 focus:outline-none"
+            />
+          </div>
           <button
             type="submit"
-            className="bg-indigo-500  px-3 py-1 rounded"
+            className="bg-indigo-500 text-white px-4 py-1 rounded"
           >
-            Search
+            <p>Search</p>
           </button>
         </div>
         <div className="flex items-center mt-4 md:mt-0 md:space-x-2 space-x-4 justify-center">
-          <div className="flex items-center space-x-3">
-            <a href="/cart" className=" relative flex items-center">
-              <TiShoppingCart size={24} /> 
-              <div className="bg-red-500 p-2.5  rounded-full w-4 h-4 flex items-center justify-center">0</div>
+          <div className="flex items-center justify-center space-x-6">
+            <a href="/cart" className="relative flex items-center">
+              <BsCart size={24} />
+              <div className="bg-indigo-500 p-2.5 rounded-full w-4 h-4 flex items-center justify-center">0</div>
               {/* cart item count */}
             </a>
           </div>
-          
+
           <button className='rounded border p-2 mt-4  md:mt-0'>Signup</button>
         </div>
       </div>
