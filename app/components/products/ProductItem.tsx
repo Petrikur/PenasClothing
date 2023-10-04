b5379b83ae9b9cbe8611fce2947cd51a711d18f6
petrikur <patekuronen123@gmail.com>
2023-10-04 17:27:47 +0300

    fix image fitting

--- app/components/products/ProductItem.tsx
+++ app/components/products/ProductItem.tsx
@@ -5,8 +5,8 @@ const ProductItem = (props: Product) => {
   return (
     <div className="card w-96 bg-base-100 shadow-xl hover:transform transition duration-300 hover:scale-105 cursor-pointer">
       <figure>
-        <img src={props.image} className="object-cover w-full h-48" alt="Shoes" />
-      </figure>
+        <img src={props.image} className="object-contain max-w-sm h-48" alt="Shoes" />
+        </figure>
       <div className="card-body flex flex-col justify-between">
         <div>
           <h2 className="card-title mb-2">{props.title}</h2>
