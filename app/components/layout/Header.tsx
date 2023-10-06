2cce14ec5e8328d43db38115f645760087c3c7a2
petrikur <patekuronen123@gmail.com>
2023-10-06 11:07:44 +0300

    fix nav

--- app/components/layout/Header.tsx
+++ app/components/layout/Header.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
-import { BsCart } from 'react-icons/bs';
-import { FaSearch } from 'react-icons/fa';
+import React from "react";
+import { BsCart } from "react-icons/bs";
+import { FaSearch } from "react-icons/fa";
+import Link from "next/link";
 
 interface HeaderProps {}
 
@@ -8,14 +9,20 @@ const Header: React.FC<HeaderProps> = () => {
   return (
     <header className="p-4">
       <div className="container mx-auto flex flex-col md:flex-row justify-between items-center">
-        <div className="logo  text-2xl font-bold text-center md:text-left">
-          Penas Clothing
-        </div>
+        <h1 className="logo text-2xl font-bold text-center md:text-left">
+          <Link href={"/"}>Penas Clothing</Link>
+        </h1>
         <nav className="md:block hidden">
           <ul className="flex space-x-6">
-            <li><a href="/">Home</a></li>
-            <li><a href="/shop">Shop</a></li>
-            <li><a href="/categories">Categories</a></li>
+            <li>
+              <a href="/">Home</a>
+            </li>
+            <li>
+              <a href="/shop">Shop</a>
+            </li>
+            <li>
+              <a href="/categories">Categories</a>
+            </li>
           </ul>
         </nav>
         <div className="flex items-center space-x-2 mt-4 md:mt-0 md:ml-4">
@@ -35,15 +42,24 @@ const Header: React.FC<HeaderProps> = () => {
           </button>
         </div>
         <div className="flex items-center mt-4 md:mt-0 md:space-x-2 space-x-4 justify-center">
-          <div className="flex items-center justify-center space-x-6">
+         
+
+          <Link
+            href={"/login"}
+            className="rounded p-2 mt-4  md:mt-0 hover:bg-gray-100"
+          >
+            Signup
+          </Link>
+
+          <div className="flex items-center justify-center space-x-6 ">
             <a href="/cart" className="relative flex items-center">
-              <BsCart size={24} />
-              <div className="bg-indigo-500 p-2.5 rounded-full w-4 h-4 flex items-center justify-center">0</div>
+              <BsCart size={20} />
+              <div className="bg-indigo-500 p-2.5 rounded-full w-4 h-4 flex items-center justify-center">
+                0
+              </div>
               {/* cart item count */}
             </a>
           </div>
-
-          <button className='rounded p-2 mt-4  md:mt-0 hover:bg-gray-100'>Signup</button>
         </div>
       </div>
     </header>
